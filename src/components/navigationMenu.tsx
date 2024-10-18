71fef474b54fdd526edbda59e19d67f26d7d7c99
Tea <tea.alushi@joinrs.com>
2024-10-18 22:22:42 +0200

    responsive navigation menu

--- src/components/navigationMenu.tsx
+++ src/components/navigationMenu.tsx
@@ -1,8 +1,12 @@
+"use client";
+
+import { XMarkIcon, Bars3Icon } from "@heroicons/react/20/solid";
 import Link from "next/link";
+import { useState } from "react";
 // import { useState } from "react";
 
 const NavigationMenu = () => {
-	// const [nav, setNav] = useState(false);
+	const [showNav, setShowNav] = useState(false);
 
 	const links = [
 		{
@@ -33,19 +37,53 @@ const NavigationMenu = () => {
 	];
 
 	return (
-		<div className="font-poppins p-3 absolute top-0 w-full flex flex-row justify-between">
-			<div>LOGO</div>
-			<div className="flex flex-row gap-5">
-				{links.map((link) => (
-					<Link
-						key={link.id}
-						href={link.link}
-						className="hover:scale-105 hover:font-semibold transition-all shadow-sm">
-						<p>{link.name}</p>
-					</Link>
-				))}
+		<>
+			<div
+				className={`${
+					showNav ? "h-80" : " h-16"
+				} md:hidden p-3 absolute top-0 w-screen flex justify-between align-middle bg-black bg-opacity-70 transition-all duration-300`}>
+				<div className="text-2xl text-pink-300 opacity-70">LOGO</div>
+				<>
+					<div
+						className={`${
+							showNav ? "opacity-100 h-full" : "opacity-0 -z-20 h-0"
+						} flex flex-col gap-8 p-8 transition-all duration-700`}>
+						{links.map((link) => (
+							<Link
+								key={link.id}
+								href={link.link}
+								className="active:scale-105 active:font-semibold active:text-pink-300 transition-all shadow-sm text-center uppercase text-gray-300 opacity-80">
+								<p>{link.name}</p>
+							</Link>
+						))}
+					</div>
+					{showNav ? (
+						<XMarkIcon
+							className="w-7 h-7 text-pink-300 opacity-70"
+							onClick={() => setShowNav(!showNav)}
+						/>
+					) : (
+						<Bars3Icon
+							className="w-7 h-7 text-pink-300 opacity-70"
+							onClick={() => setShowNav(!showNav)}
+						/>
+					)}
+				</>
 			</div>
-		</div>
+			<div className="hidden font-poppins p-3 absolute top-0 w-full md:flex flex-row justify-between">
+				<div>LOGO</div>
+				<div className="flex flex-row gap-5">
+					{links.map((link) => (
+						<Link
+							key={link.id}
+							href={link.link}
+							className="hover:scale-105 hover:font-semibold transition-all shadow-sm">
+							<p>{link.name}</p>
+						</Link>
+					))}
+				</div>
+			</div>
+		</>
 	);
 };
 
