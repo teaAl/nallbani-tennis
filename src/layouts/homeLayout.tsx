a3df1e00a11d688d5fb365f642755175fb5cc12d
Tea Alushi <tea.alushi@gmail.com>
2025-04-10 17:53:12 +0200

    fix build

--- src/layouts/homeLayout.tsx
+++ src/layouts/homeLayout.tsx
@@ -1,12 +1,15 @@
 import React from "react";
 import NavigationMenu from "@/components/common/navbar/navigationMenu";
 import Footer from "@/components/common/footer";
+import { authOptions } from '@/lib/authOpts';
+import { getServerSession } from 'next-auth';
 
-const Layout = ({ children }: { children: React.ReactNode }) => {
+async function Layout({ children }: { children: React.ReactNode })  {
+	const session = await getServerSession(authOptions);
 	return (
 		<>
 			<div className="flex flex-col min-h-screen h-full">
-				<NavigationMenu />
+				<NavigationMenu session={session}/>
 				<main className="/*min-h-screen*/ /*h-screen*/ flex-grow pt-16 shadow-md">
 					{children}
 				</main>
@@ -17,3 +20,7 @@ const Layout = ({ children }: { children: React.ReactNode }) => {
 };
 
 export default Layout;
+
+/*
+TODO: Not needed, delete this file after revising booking journey for guests.
+*/
