b87cf95718d935b17493607ffa50b9b8ca9578fb
Nadia Mezini <nadia.mezini@italiacoding.com>
2026-01-15 12:33:41 +0100

    Translate Services page; SSPut real data in services section.

--- src/components/services/info.tsx
+++ src/components/services/info.tsx
@@ -3,36 +3,28 @@
 import Lottie from "lottie-react";
 import { CalendarCheck2, IdCard, User, Users } from "lucide-react";
 import singlePlayerLottie from "../../../public/animations/singleplayer.json";
+import { useTranslations } from "next-intl";
 
 const ServicesInfo = () => {
+    const t = useTranslations('ServicesPage');
+  
   return (
     <div className="flex flex-col gap-10 justify-between items-center">
       <div className="flex flex-col gap-4 justify-around ">
         <h3
           className={`text-gray-900 bg-pear font-semibold md:text-2xl text-xl text-center font-poppins w-max`}
         >
-          A Variety of Services
+          {t('Section1.title')}
           {/* Be Part of Our Community */}
         </h3>
         <p className="text-foreground font-poppins font-light">
-          At Nallbani Tennis, we believe in providing a comprehensive experience
-          for our members. Our services are designed to cater to players of all
-          levels, from beginners to advanced. Our coaching programs are designed
-          to be fun and engaging, ensuring that you not only improve your skills
-          but also enjoy the process.
+          {t('Section1.p1')}
         </p>
         <p className="text-foreground font-poppins font-light">
-          In addition to coaching, we offer a range of membership options to
-          suit your needs. We also organize regular events and tournaments,
-          giving you the opportunity to showcase your skills and compete with
-          other players. These events are a great way to meet new people, make
-          friends, and enjoy the social aspect of the game.
+          {t('Section1.p2')}
         </p>
         <p className="text-foreground font-poppins font-light">
-          At Nallbani Tennis, we are committed to creating a welcoming and
-          inclusive community for all players. We believe that tennis is not
-          just a sport, but a way to connect with others and build lasting
-          relationships.
+          {t('Section1.p3')}
         </p>
       </div>
       <div className="w-screen md:p-12 p-4 bg-gradient-to-r from-pear via-gray-800 to-gray-800">
@@ -42,14 +34,12 @@ const ServicesInfo = () => {
               <div className="flex flex-row gap-4 justify-between items-center border-b border-foreground/10 pb-4">
                 <User className="text-pear w-7 h-7" />
                 <p className="text-pear font-poppins font-light">
-                  Individual Lessons
+                  {t('Section2.title1')}
                 </p>
               </div>
               <div>
                 <p className="text-foreground/70 font-poppins font-light text-sm text-right">
-                  lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Quisquam voluptatibus, quibusdam, voluptates, quis, quas.
-                  Quisquam
+                  {t('Section2.p1')}
                 </p>
               </div>
             </div>
@@ -59,14 +49,12 @@ const ServicesInfo = () => {
               <div className="flex flex-row gap-4 justify-between items-center border-b border-foreground/10 pb-4">
                 <Users className="text-pear w-7 h-7" />
                 <p className="text-pear font-poppins font-light">
-                  Group Lessons
+                  {t('Section2.title2')}
                 </p>
               </div>
               <div>
                 <p className="text-foreground/70 font-poppins font-light text-sm text-right">
-                  lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Quisquam voluptatibus, quibusdam, voluptates, quis, quas.
-                  Quisquam
+                  {t('Section2.p2')}
                 </p>
               </div>
             </div>
@@ -76,14 +64,12 @@ const ServicesInfo = () => {
               <div className="flex flex-row gap-4 justify-between items-center border-b border-foreground/10 pb-4">
                 <IdCard className="text-pear w-7 h-7" />
                 <p className="text-pear font-poppins font-light">
-                  Membership Options
+                  {t('Section2.title3')}
                 </p>
               </div>
               <div>
                 <p className="text-foreground/70 font-poppins font-light text-sm text-right">
-                  lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Quisquam voluptatibus, quibusdam, voluptates, quis, quas.
-                  Quisquam
+                  {t('Section2.p3')}
                 </p>
               </div>
             </div>
@@ -93,14 +79,12 @@ const ServicesInfo = () => {
               <div className="flex flex-row gap-4 justify-between items-center border-b border-foreground/10 pb-4">
                 <CalendarCheck2 className="text-pear w-7 h-7" />
                 <p className="text-pear font-poppins font-light">
-                  Court Reservations
+                  {t('Section2.title4')}
                 </p>
               </div>
               <div>
                 <p className="text-foreground/70 font-poppins font-light text-sm text-right">
-                  lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Quisquam voluptatibus, quibusdam, voluptates, quis, quas.
-                  Quisquam
+                  {t('Section2.p4')}
                 </p>
               </div>
             </div>
